f44f56e0d77a369b39a5ded49c91be6fe0df97a6
srikar <developer.srikarbojji@gmail.com>
2023-07-06 01:31:04 +0530

    Add project files.

---
+++ EntityDbContext/DatabaseProviders/PostgreSqlProvider.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityDbContext.DatabaseProviders;
+
+public class PostgreSqlProvider : IDatabaseProvider
+{
+    public void Configure(DbContextOptionsBuilder optionsBuilder, string connectionString)
+    {
+        optionsBuilder.UseNpgsql(connectionString);
+    }
+}
---
+++ EntityDbContext/KarmachariDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityDbContext;
+
+public class KarmachariDbContext : DbContext
+{
+    private readonly string _connectionString;
+    private readonly IDatabaseProvider _databaseProvider;
+
+    public KarmachariDbContext(DbContextOptions<KarmachariDbContext> options, IConfiguration configuration, IDatabaseProvider databaseProvider)
+         : base(options)
+    {
+        _connectionString = configuration.GetConnectionString("DefaultConnection");
+        _databaseProvider = databaseProvider;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            _databaseProvider.Configure(optionsBuilder, _connectionString);
+        }
+    }
+}
